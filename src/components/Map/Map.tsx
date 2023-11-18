b00dd7f421031b7c9d70c197fb8d5c6f2b48a6f2
Danber <bergauzen.danil@mail.ru>
2023-11-18 17:20:32 -0500

    Added yandex map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { YMaps } from "@pbe/react-yandex-maps";
+import { Map as YMap } from "@pbe/react-yandex-maps";
 
 import styles from "./Map.module.scss";
 
 const Map = () => {
-  return <div className={styles.map}>MAP</div>;
+  return (
+    <div className={styles.mapContainer}>
+      <YMaps>
+        <YMap
+          className={styles.map}
+          defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+        />
+      </YMaps>
+    </div>
+  );
 };
 
 export default Map;
