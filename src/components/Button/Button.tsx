0f5fb000934b607343f695aafeba398a30962254
Danber <steem.sobol@mail.ru>
2023-11-20 16:30:07 -0500

    Added className in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,11 +6,12 @@ interface ButtonProps {
   children: React.ReactNode;
   image?: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 }
 
-const Button = ({ children, image, onClick }: ButtonProps) => {
+const Button = ({ children, image, onClick, className }: ButtonProps) => {
   return (
-    <div className={styles.button} onClick={onClick}>
+    <div className={`${styles.button} ${className}`} onClick={onClick}>
       {image}
       <div>{children}</div>
     </div>
