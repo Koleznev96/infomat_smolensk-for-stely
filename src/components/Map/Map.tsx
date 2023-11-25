a850226d38b5f1ef04acb9d93a418d51a8221c0e
Danber <steem.sobol@mail.ru>
2023-11-24 21:36:03 -0500

    Added yandex map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,18 +1,39 @@
-import React from "react";
-import { YMaps } from "@pbe/react-yandex-maps";
-import { Map as YMap } from "@pbe/react-yandex-maps";
+import React, { useEffect, useRef } from "react";
 
 import styles from "./Map.module.scss";
 
+// @ts-ignore
+const ymaps: any = window.ymaps;
+
 const Map = () => {
+  const refMap = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    ymaps.ready(() => {
+      const myMap = new ymaps.Map(refMap.current, {
+        center: [55.751574, 37.573856],
+        zoom: 10,
+        controls: [],
+      });
+
+      const multiRoute = new ymaps.multiRouter.MultiRoute({
+        referencePoints: [
+          [55.73447, 37.58],
+          [55.734336, 37.51218],
+          [55.724102, 37.19912],
+        ],
+        params: {
+          routingMode: "pedestrian",
+        },
+      });
+
+      myMap.geoObjects.add(multiRoute);
+    });
+  }, []);
+
   return (
     <div className={styles.mapContainer}>
-      <YMaps>
-        <YMap
-          className={styles.map}
-          defaultState={{ center: [55.75, 37.57], zoom: 9 }}
-        />
-      </YMaps>
+      <div ref={refMap} className={styles.map}></div>
     </div>
   );
 };
