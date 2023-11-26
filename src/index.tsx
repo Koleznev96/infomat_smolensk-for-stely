88d92dc9903258db0ca8ccf126b46c8874acd264
Danber <steem.sobol@mail.ru>
2023-11-25 21:41:46 -0500

    Return StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
@@ -9,4 +9,8 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
