23d5f3c691eda63896ceeab6ae15cb9cd76a97f2
Danber <steem.sobol@mail.ru>
2023-12-12 14:22:49 -0500

    Update metric

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -39,7 +39,6 @@ import {
 } from "src/hooks";
 
 import styles from "./Header.module.scss";
-import { YMInitializer } from "react-yandex-metrika";
 
 type Type = "events" | "places";
 
@@ -123,6 +122,49 @@ const Header = () => {
   }, [debounceValue, events, places]);
 
   useEffect(() => {
+    if (response?.data?.yandexMetricCode) {
+      const script = document.createElement("script");
+      const scriptBody = document.createTextNode(`
+      (function (m, e, t, r, i, k, a) {
+        m[i] =
+          m[i] ||
+          function () {
+            (m[i].a = m[i].a || []).push(arguments);
+          };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) {
+            return;
+          }
+        }
+        (k = e.createElement(t)),
+          (a = e.getElementsByTagName(t)[0]),
+          (k.async = 1),
+          (k.src = r),
+          a.parentNode.insertBefore(k, a);
+      })(
+        window,
+        document,
+        "script",
+        "https://mc.yandex.ru/metrika/tag.js",
+        "ym",
+      );
+
+      window.ym(${response?.data?.yandexMetricCode}, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+      });
+    `);
+
+      script.appendChild(scriptBody);
+
+      document.head.append(script);
+    }
+  }, [response?.data?.yandexMetricCode]);
+
+  useEffect(() => {
     if (!debounceValue) {
       setSearchObjects([]);
       return;
@@ -193,148 +235,133 @@ const Header = () => {
   };
 
   return (
-    <>
-      <YMInitializer
-        accounts={[Number(response?.data?.yandexMetricCode)]}
-        options={{
-          clickmap: true,
-          trackLinks: true,
-          accurateTrackBounce: true,
-          webvisor: true,
-          trackHash: true,
-        }}
-        version="2"
-      />
-      <div className={styles.header}>
-        <div className={styles.headerContent}>
-          <div className={styles.date}>
+    <div className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.date}>
+          <span>
+            {date.toLocaleDateString(language.replace("_", "-"), {
+              timeZone: "Europe/Moscow",
+              weekday: "long",
+            })}
+            ,{" "}
             <span>
               {date.toLocaleDateString(language.replace("_", "-"), {
                 timeZone: "Europe/Moscow",
-                weekday: "long",
               })}
-              ,{" "}
-              <span>
-                {date.toLocaleDateString(language.replace("_", "-"), {
-                  timeZone: "Europe/Moscow",
-                })}
-              </span>
             </span>
-            <span>{moscowTime}</span>
-          </div>
-          <div className={styles.top}>
-            <div className={styles.weather}>
-              <img
-                src={language === "ru_RU" ? yandexRU : yandexEN}
-                width={127}
-                height={34}
-                alt="yandex-weather"
-              />
-              <span className={styles.info}>
-                <span>
-                  {languageControl("Смоленск", "Smolensk")}
-                  {weather?.fact?.icon && (
-                    <SVGIcon path={weather?.fact?.icon} />
-                  )}
-                  {weather?.fact?.temp || 0}°C
-                </span>
-                <span className={styles.blur}>
-                  {currentTimeOfDay(weather?.forecast?.parts[0])}°C
-                </span>
-                <span className={styles.blur}>
-                  {currentTimeOfDay(weather?.forecast?.parts[1])}°C
-                </span>
-              </span>
-            </div>
-            <div className={styles.language}>
-              <span
-                onClick={() => changeLanguage("ru_RU")}
-                className={language === "ru_RU" ? styles.active : ""}
-              >
-                <RU />
-                РУ
-              </span>
-              <span
-                onClick={() => changeLanguage("en_US")}
-                className={language === "en_US" ? styles.active : ""}
-              >
-                <GB />
-                EN
-              </span>
-            </div>
-          </div>
+          </span>
+          <span>{moscowTime}</span>
         </div>
-        <div className={styles.headerMiddle}>
-          <img src={logo} alt="logo" />
-          <div className={styles.info}>
-            <h3>
-              {languageControl(response?.data?.title, response?.data?.titleEn)}
-            </h3>
-            <p>{title}</p>
-          </div>
-          <img className={styles.backgroundImage} src={headBG} alt="head-bg" />
-        </div>
-        <div className={styles.headerBottom}>
-          <div
-            className={styles.content}
-            style={{
-              boxShadow:
-                searchObjects?.length >= 1
-                  ? "0 0 0 6px #edeff2, 0 1px 2px 0 rgba(16, 24, 40, 0.05)"
-                  : "",
-            }}
-          >
-            <span>
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M17.5 17.5L13.875 13.875M15.8333 9.16667C15.8333 12.8486 12.8486 15.8333 9.16667 15.8333C5.48477 15.8333 2.5 12.8486 2.5 9.16667C2.5 5.48477 5.48477 2.5 9.16667 2.5C12.8486 2.5 15.8333 5.48477 15.8333 9.16667Z"
-                  stroke="#667085"
-                  strokeWidth="1.66667"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </span>
-            <input
-              type="text"
-              placeholder={languageControl("Поиск", "Search")}
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
+        <div className={styles.top}>
+          <div className={styles.weather}>
+            <img
+              src={language === "ru_RU" ? yandexRU : yandexEN}
+              width={127}
+              height={34}
+              alt="yandex-weather"
             />
-            {searchObjects?.length >= 1 ? (
-              <div className={styles.resultSection}>
-                {searchObjects.map((object, index) => (
-                  <div
-                    onClick={() => goToObject(object.link || "#")}
-                    className={styles.card}
-                    key={index}
-                  >
-                    <img src={object.image} alt={object.image} />
-                    <div className={styles.text}>
-                      <h4>{languageControl(object.title, object.titleEn)}</h4>
-                      <p>
-                        {languageControl(
-                          object.description,
-                          object.descriptionEn,
-                        )}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              ""
-            )}
+            <span className={styles.info}>
+              <span>
+                {languageControl("Смоленск", "Smolensk")}
+                {weather?.fact?.icon && <SVGIcon path={weather?.fact?.icon} />}
+                {weather?.fact?.temp || 0}°C
+              </span>
+              <span className={styles.blur}>
+                {currentTimeOfDay(weather?.forecast?.parts[0])}°C
+              </span>
+              <span className={styles.blur}>
+                {currentTimeOfDay(weather?.forecast?.parts[1])}°C
+              </span>
+            </span>
+          </div>
+          <div className={styles.language}>
+            <span
+              onClick={() => changeLanguage("ru_RU")}
+              className={language === "ru_RU" ? styles.active : ""}
+            >
+              <RU />
+              РУ
+            </span>
+            <span
+              onClick={() => changeLanguage("en_US")}
+              className={language === "en_US" ? styles.active : ""}
+            >
+              <GB />
+              EN
+            </span>
           </div>
         </div>
       </div>
-    </>
+      <div className={styles.headerMiddle}>
+        <img src={logo} alt="logo" />
+        <div className={styles.info}>
+          <h3>
+            {languageControl(response?.data?.title, response?.data?.titleEn)}
+          </h3>
+          <p>{title}</p>
+        </div>
+        <img className={styles.backgroundImage} src={headBG} alt="head-bg" />
+      </div>
+      <div className={styles.headerBottom}>
+        <div
+          className={styles.content}
+          style={{
+            boxShadow:
+              searchObjects?.length >= 1
+                ? "0 0 0 6px #edeff2, 0 1px 2px 0 rgba(16, 24, 40, 0.05)"
+                : "",
+          }}
+        >
+          <span>
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M17.5 17.5L13.875 13.875M15.8333 9.16667C15.8333 12.8486 12.8486 15.8333 9.16667 15.8333C5.48477 15.8333 2.5 12.8486 2.5 9.16667C2.5 5.48477 5.48477 2.5 9.16667 2.5C12.8486 2.5 15.8333 5.48477 15.8333 9.16667Z"
+                stroke="#667085"
+                strokeWidth="1.66667"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <input
+            type="text"
+            placeholder={languageControl("Поиск", "Search")}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          {searchObjects?.length >= 1 ? (
+            <div className={styles.resultSection}>
+              {searchObjects.map((object, index) => (
+                <div
+                  onClick={() => goToObject(object.link || "#")}
+                  className={styles.card}
+                  key={index}
+                >
+                  <img src={object.image} alt={object.image} />
+                  <div className={styles.text}>
+                    <h4>{languageControl(object.title, object.titleEn)}</h4>
+                    <p>
+                      {languageControl(
+                        object.description,
+                        object.descriptionEn,
+                      )}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
