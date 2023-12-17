a7504f572f0bc2f78ee24ff7fc3d3bc2ae86834e
Danber <bergauzen.danil@mail.ru>
2023-12-17 12:52:35 -0500

    display all monuments

--- src/routes/TouristObjects/TouristObjects.tsx
+++ src/routes/TouristObjects/TouristObjects.tsx
@@ -14,7 +14,7 @@ const TouristObjects = () => {
   const languageControl = useLanguageControl();
 
   const { data: response } = useGetCategoriesQuery(undefined);
-  const { data: places } = useGetPlacesQuery({ page: 1 });
+  const { data: places } = useGetPlacesQuery({ page: 0 });
 
   useEffect(() => {
     if (!places?.rows?.length) {
