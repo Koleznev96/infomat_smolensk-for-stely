4997e12111e0a645d0810afc8730859524578e6f
Aleksey Koleznev <fc.fenomen@mail.ru>
2024-01-15 23:44:51 +0700

    fixed zoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,6 +6,33 @@ import { index } from "src/store";
 import { ReactRoutes } from "src/routes";
 
 function App() {
+  useEffect(() => {
+    function handleGestureStart(e: Event) {
+      e.preventDefault();
+      document.body.style.transform = "scale(0.99)";
+    }
+
+    function handleGestureChange(e: Event) {
+      e.preventDefault();
+      document.body.style.transform = "scale(0.99)";
+    }
+
+    function handleGestureEnd(e: Event) {
+      e.preventDefault();
+      document.body.style.transform = "scale(1)";
+    }
+
+    document.addEventListener("gesturestart", handleGestureStart);
+    document.addEventListener("gesturechange", handleGestureChange);
+    document.addEventListener("gestureend", handleGestureEnd);
+
+    return () => {
+      document.removeEventListener("gesturestart", handleGestureStart);
+      document.removeEventListener("gesturechange", handleGestureChange);
+      document.removeEventListener("gestureend", handleGestureEnd);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Provider store={index}>
